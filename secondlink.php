8e362cb2841c5f545e013729450b378911365b7e
medha-harimash <harimash77@gmail.com>
2021-01-12 18:43:28 +0530

    my files

---
+++ secondlink.php
@@ -0,0 +1,8 @@
+<div class="header sticky">
+        <a href="#mysite" class="logo"><img src="images/logo1.png" alt=""></a>
+        <div class="header-right">
+          <a class="active" href="#home">Home</a>
+          <a href="#contact">Contact</a>
+          <a href="#about">About</a>
+        </div>
+      </div>
